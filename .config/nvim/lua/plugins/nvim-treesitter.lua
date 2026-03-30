c75c3bc16bd63bdc82513fa288b98213ae8176e7
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2026-03-30 15:36:28 -0500

    update treesitter structure

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,15 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { "BufReadPost", "BufNewFile" },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ':TSUpdate',
+  event = { "BufReadPost", "BufNewFile" },
   config = function()
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "html", "css", "json", "bash", "vue", "tsx", "markdown", "markdown_inline", "astro" },
+    require("nvim-treesitter").setup({
       sync_install = false,
       auto_install = true,
       highlight = { enable = true, additional_vim_regex_highlighting = false },
       indent = { enable = true },
-    }
+    })
+    require("nvim-treesitter").install({ "lua", "vim", "vimdoc", "query", "python", "javascript", "typescript", "html", "css", "json", "bash", "vue", "tsx", "markdown", "markdown_inline", "astro" })
   end
 }
