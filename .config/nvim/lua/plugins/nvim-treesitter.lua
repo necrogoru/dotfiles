0e24e1358ec1c4899b0e928c7cc9b8e18e325a3b
Jhon Fredy Berrio Marin <jhoxsan@gmail.com>
2026-03-31 15:50:55 -0500

    chore: centralize parsers for treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,11 +3,7 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-    require("nvim-treesitter").setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-    })
-
-    require("nvim-treesitter").install({
+    local parsers = {
       "lua",
       "vim",
       "vimdoc",
@@ -24,29 +20,23 @@ return {
       "markdown",
       "markdown_inline",
       "astro",
+    }
+
+    local filetypes = vim.list_extend(vim.deepcopy(parsers), {
+      "typescriptreact",
+      "javascriptreact",
     })
+
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    require("nvim-treesitter").install(parsers)
 
     local treesitter_group = vim.api.nvim_create_augroup("UserTreesitter", { clear = true })
     vim.api.nvim_create_autocmd("FileType", {
       group = treesitter_group,
-      pattern = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "python",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "json",
-        "bash",
-        "vue",
-        "typescriptreact",
-        "javascriptreact",
-        "markdown",
-        "astro",
-      },
+      pattern = filetypes,
       callback = function(args)
         pcall(vim.treesitter.start, args.buf)
       end,
